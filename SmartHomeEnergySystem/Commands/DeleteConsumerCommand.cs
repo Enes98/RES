f705f9bdc8814f543d2ee3acb7617f32a600fc80
Sladja98 <62813776+Sladja98@users.noreply.github.com>
2020-06-07 16:53:01 +0200

    Consumers Delete and Add(update) modified

---
+++ SmartHomeEnergySystem/Commands/DeleteConsumerCommand.cs
@@ -0,0 +1,25 @@
+﻿using SmartHomeEnergySystem.Models;
+using SmartHomeEnergySystem.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHomeEnergySystem.Commands
+{
+    public class DeleteConsumerCommand : ICommand
+    {
+        private ConsumerModel consumerToDelete;
+
+        public DeleteConsumerCommand(ConsumerModel consumerToDelete)
+        {
+            this.consumerToDelete = consumerToDelete;
+        }
+
+        public void Execute()
+        {
+            ConsumerViewModel.consumers.Remove(consumerToDelete);   
+        }
+    }
+}
--- SmartHomeEnergySystem/ViewModels/ConsumerViewModel.cs
+++ SmartHomeEnergySystem/ViewModels/ConsumerViewModel.cs
@@ -1,4 +1,6 @@
-﻿using SmartHomeEnergySystem.Models;
+﻿using SmartHomeEnergySystem.Command;
+using SmartHomeEnergySystem.Commands;
+using SmartHomeEnergySystem.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -12,16 +14,24 @@ namespace SmartHomeEnergySystem.ViewModels
     {
         public static ObservableCollection<ConsumerModel> consumers { get; set; }
         private ConsumerModel selectedConsumer;
+
+        public static MyICommand DeleteConsumerCommand { get; set; }
+
         
         public ConsumerModel SelectedConsumer
         {
             get { return selectedConsumer; }
-            set { selectedConsumer = value; }
+            set
+            {
+                selectedConsumer = value;
+                DeleteConsumerCommand.RaiseCanExecuteChanged();
+            }
         }
 
         public ConsumerViewModel()
         {
             loadConsumers();
+            DeleteConsumerCommand = new MyICommand(OnDeleteConsumer, CanDeleteConsumer);
         }
 
         private void loadConsumers()
@@ -31,6 +41,17 @@ namespace SmartHomeEnergySystem.ViewModels
             consumers.Add(new ConsumerModel("TV", 20.22));
             consumers.Add(new ConsumerModel("Air-conditioner", 100.6));
         }
+        private bool CanDeleteConsumer()
+        {
+            return SelectedConsumer != null;
+        }
+
+        private void OnDeleteConsumer()
+        {
+            DeleteConsumerCommand deleteCommandC = new DeleteConsumerCommand(SelectedConsumer);
+            deleteCommandC.Execute();
+        }
+
 
         /*
         //public static ObservableCollection<ConsumerModel> Consumers { get; set; }
