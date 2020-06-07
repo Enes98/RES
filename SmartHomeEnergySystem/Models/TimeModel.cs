2b74e243df3fc169f34f46cd31502921a6dea589
Sladja98 <62813776+Sladja98@users.noreply.github.com>
2020-06-08 00:21:37 +0200

    Time start

---
+++ SmartHomeEnergySystem/Models/TimeModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHomeEnergySystem.Models
+{
+    public class TimeModel
+    {
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+        public TimeModel()
+        {
+            Hour = 0;
+            Minute = 0;
+            Second = 0;
+        }
+
+    }
+}
